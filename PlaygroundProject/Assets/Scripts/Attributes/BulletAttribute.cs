08484005af8329e982f2fdfaf457abfe6cc0d38b
Douglas Urner <dlu@canishe.com>
2018-05-13 22:20:31 -0700

    Initial commit.

---
+++ PlaygroundProject/Assets/Scripts/Attributes/BulletAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Playground/Attributes/Bullet")]
+public class BulletAttribute : MonoBehaviour
+{
+	[HideInInspector]
+	public int playerId;
+}
